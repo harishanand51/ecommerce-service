154583eebf7aa0a9d3e750fc22e71e531d7e65a9
manideepika <manideepikaadusumilli36@gmail.com>
2023-11-06 01:46:23 -0500

    home component

--- FrontEnd/ecommerce/src/home/homepage/homepage.component.ts
+++ FrontEnd/ecommerce/src/home/homepage/homepage.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { PageEvent } from '@angular/material/paginator';
+
 
 @Component({
   selector: 'app-homepage',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent {
+  cardDataList=[{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}];
+  page:number = 1; // Initial page
+  pageSize = 5; // Initial page size
+  searchForm = new FormGroup({
+    searchInput: new FormControl(''),
+  });
+  search(){
+    const searchTerm = this.searchForm.controls.searchInput.value
+    // You can perform your search logic here, e.g., make an API request or filter data.
+    console.log('Search term:', searchTerm);
+  }
+  addtoCart(){
 
+  }
+  product_details(){}
+  getVisibleCards() {
+    const startIndex = (this.page - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    return this.cardDataList.slice(startIndex, endIndex);
+  }
+  onPageChange(event: PageEvent) {
+    this.page = event.pageIndex + 1;
+    this.pageSize = event.pageSize;
+    this.getVisibleCards();
+  }
 }
+
+
