c4d515ebe41c7c445903058170650377af6cf2e5
supriyaedamalapati <supriyaedamalapati72@gmail.com>
2023-11-25 17:30:17 -0500

    registration

--- FrontEnd/ecommerce/src/login/registration/registration.module.ts
+++ FrontEnd/ecommerce/src/login/registration/registration.module.ts
@@ -6,13 +6,14 @@ import {Component, Inject} from '@angular/core';
 import {MatDialog, MAT_DIALOG_DATA, MatDialogRef, MatDialogModule} from '@angular/material/dialog';
 import {NgIf} from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
 import { RegisterpageComponent } from './registerpage/registerpage.component';
-
-
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatSelectModule} from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -28,7 +29,11 @@ import { RegisterpageComponent } from './registerpage/registerpage.component';
     MatButtonModule,
     NgIf,
     MatDialogModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatStepperModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatIconModule
   ],
   exports:[
     LoginpageComponent,
